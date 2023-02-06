b58eab3e875124307fc84c1eefd19ddfe22d9ade
sbhrwl <sabharwalonnet@gmail.com>
2023-02-06 12:57:05 +0200

    Update Pangram.java

--- basics2/exercises/Pangram.java
+++ basics2/exercises/Pangram.java
@@ -5,14 +5,19 @@ public class Pangram
        boolean flag=false;
         String str="THE QUICK ROWN FOX JUMPS OVER LAZY DOG";
         str=str.replace(" ", "");
+        
+        // Convert string to array of characters
         char []ch=str.toCharArray();
-
+        // Create an empty array of length 26 (equivalent to 26 alphabets)
         int ar[]=new int[26];
 
+        // Traverse character array and initilise with ASCII decimal equivalent of alphabets
         for(int i=0;i<ch.length;i++)
         {
-            ar[ch[i]-65]++;
+            ar[ch[i]-65]++; // ch[i]: alphabet in character array
         }
+        
+        // Check if there is a missing aplphabet
         for(int i=0;i<ar.length;i++)
         {
             if(ar[i]==0)
@@ -25,10 +30,8 @@ public class Pangram
         if(flag==false)
         {
             System.out.println("Its pangram");
-        }
-        
-        
-    
+        }        
+   
     }
     
 }
